b438850f3eea0c740293f4429a11a2c815483f43
fred@griffin <frederic.zhang@adelaide.edu.au>
2024-03-30 17:49:34 +1030

    add a few more arguments

--- src/args.py
+++ src/args.py
@@ -43,6 +43,18 @@ def parse_arguments():
         choices=["entropy", "cross_entropy"]
     )
     parser.add_argument(
+        "--blockwise-coef",
+        default=False,
+        action="store_true",
+        help="Use different coefficients on different parameter blocks."
+    )
+    parser.add_argument(
+        "--partition",
+        default="trainval",
+        choices=["trainval", "traintest"],
+        help="Partitions of the dataset to use."
+    )
+    parser.add_argument(
         "--train-dataset",
         default=None,
         type=lambda x: x.split(","),
