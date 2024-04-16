c4cbec0d58164b327b88c8b10c64b9203c04d473
fred@griffin <frederic.zhang@adelaide.edu.au>
2024-04-16 10:24:20 +0930

    relocate list of buffers to designated device

--- src/composition.py
+++ src/composition.py
@@ -10,7 +10,7 @@ from torch import nn
 from functorch import jvp, make_functional_with_buffers
     
 class WeightedImageEncoder(nn.Module):
-    def __init__(self, model, task_vectors, device, blockwise=True) -> None:
+    def __init__(self, model, task_vectors, blockwise=True) -> None:
         """A wrapper class to enable compositions of task vectors
 
         Parameter:
@@ -19,8 +19,6 @@ class WeightedImageEncoder(nn.Module):
             CLIP image encoder model.
         task_vectors: List[NonLinearTaskVector]
             List of task vectors to learn coefficients for.
-        device: str or int
-            Device to relocate the task vectors to.
         blockwise: bool, default: True
             Learn a coefficient for each parameter block.
         """
@@ -29,29 +27,33 @@ class WeightedImageEncoder(nn.Module):
         func, params, self.buffer = make_functional_with_buffers(model)
         # NOTE This is important to avoid the following error
         # NotImplementedError: Cannot copy out of meta tensor; no data!
-        self.func = lambda p, x: func(p, self.buffer, x)
+        self.func = lambda p, b, x: func(p, b, x)
         self.params = torch.nn.ParameterList(params)
         for p in self.params:
             p.requires_grad = False
 
-        self.device = device
         # Copy the attributes from the image encoder.
         self.train_preprocess = model.train_preprocess
         self.val_preprocess = model.val_preprocess
         self.cache_dir = model.cache_dir
 
-        dparams = []
-        for tv in task_vectors:
-            dp = [tv.vector[k].to(device) for k in tv.vector]
-            dparams.append(dp)
-
-        self.dparams = dparams
-
+        self.dparams = [[tv[k] for k in tv] for tv in task_vectors]
         self.blockwise = blockwise
         if blockwise:
             self.coef = torch.nn.Parameter(torch.zeros(len(task_vectors), len(self.params)))
         else:
             self.coef = torch.nn.Parameter(torch.zeros(len(task_vectors),))
+
+    def _apply(self, fn):
+        """Override method to relocate buffer list
+
+        NOTE: This function signature is for PyTorch 1.13.1.
+        Newer verions have added another optional argument `recurse=True`.
+        """
+        new_self = super()._apply(fn=fn)
+        new_self.buffer = (fn(x) for x in new_self.buffer)
+        new_self.dparams = [[fn(tv[k]) for k in tv] for tv in new_self.dparams]
+        return new_self
 
     def forward(self, x) -> torch.Tensor:
         if self.blockwise:
@@ -59,10 +61,10 @@ class WeightedImageEncoder(nn.Module):
         else:
             dparams = [sum([p * c for p, c in zip(dp, self.coef)]) for dp in zip(*self.dparams)]
         new_params = [dp + p for dp, p in zip(dparams, self.params)]
-        return self.func(new_params, x)
+        return self.func(new_params, self.buffer, x)
 
 class WeightedLinearizedModel(nn.Module):
-    def __init__(self, model, task_vectors, device, blockwise=True) -> None:
+    def __init__(self, model, task_vectors, blockwise=True) -> None:
         """A wrapper class to enable compositions of task vectors for linearised models
         
         Parameters:
@@ -71,8 +73,6 @@ class WeightedLinearizedModel(nn.Module):
             Linearised model using first-order Taylor expansion.
         task_vectors: List[LinearizedTaskVector]
             List of task vectors to learn coefficients for.
-        device: str or int
-            Device to relocate the task vectors to.
         blockwise: bool, default: True
             Learn a coefficient for each parameter block.
         """
@@ -83,19 +83,23 @@ class WeightedLinearizedModel(nn.Module):
         self.buffers0 = model.buffers0
         self._model_name = model._model_name
 
-        self.device = device
-
-        dparams = []
-        for tv in task_vectors:
-            dp = [tv.vector[k].to(device) for k in tv.vector if k.startswith('model.params.')]
-            dparams.append(dp)
-
-        self.dparams = dparams
+        self.dparams = [[tv[k] for k in tv] for tv in task_vectors]
         self.blockwise = blockwise
         if blockwise:
             self.coef = torch.nn.Parameter(torch.zeros(len(task_vectors), len(self.params0)))
         else:
             self.coef = torch.nn.Parameter(torch.zeros(len(task_vectors),))
+
+    def _apply(self, fn):
+        """Override method to relocate buffer list
+
+        NOTE: This function signature is for PyTorch 1.13.1.
+        Newer verions have added another optional argument `recurse=True`.
+        """
+        new_self = super()._apply(fn=fn)
+        new_self.buffers0 = (fn(x) for x in new_self.buffers0)
+        new_self.dparams = [[fn(tv[k]) for k in tv] for tv in new_self.dparams]
+        return new_self
 
     def forward(self, x) -> torch.Tensor:
         if self.blockwise:
@@ -103,7 +107,7 @@ class WeightedLinearizedModel(nn.Module):
         else:
             dparams = [sum([p * c for p, c in zip(dp, self.coef)]) for dp in zip(*self.dparams)]
         out, dp = jvp(
-            lambda param: self.func0(param, x),
+            lambda param: self.func0(param, self.buffers0, x),
             (tuple(self.params0),),
             (tuple(dparams),),
         )
